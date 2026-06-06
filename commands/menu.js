5c4b3c6c6422f7c1cf28f90d68b57db822a48bec
Still <steveayoti01@gmail.com>
2026-06-06 17:46:16 +0300

    Add bot menu command with available options

--- commands/menu.js
+++ commands/menu.js
@@ -1,1 +1,11 @@
+module.exports = async (sock, sender) => {
+  await sock.sendMessage(sender, {
+    text: `
+🦅 NIGHT HAWK BOT MENU
 
+Commands:
+.ping - check bot status
+.menu - show this menu
+`
+  });
+};

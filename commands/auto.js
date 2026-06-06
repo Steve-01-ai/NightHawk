4b651e9faca590a7fb19f1cf2228304b18d8ed26
Still <steveayoti01@gmail.com>
2026-06-06 20:20:03 +0300

    Update auto.js

--- commands/auto.js
+++ commands/auto.js
@@ -1,1 +1,34 @@
+const auto = {
+  autoread: true,
+  autotyping: false,
+  autorecord: false,
+  autostatus: true
+};
 
+module.exports = {
+
+  ".autoread": async (sock, msg, text, sender) => {
+    auto.autoread = !auto.autoread;
+
+    return sock.sendMessage(sender, {
+      text: `👁️ AutoRead: ${auto.autoread ? "ON" : "OFF"}`
+    });
+  },
+
+  ".autotyping": async (sock, msg, text, sender) => {
+    auto.autotyping = !auto.autotyping;
+
+    return sock.sendMessage(sender, {
+      text: `⌨️ AutoTyping: ${auto.autotyping ? "ON" : "OFF"}`
+    });
+  },
+
+  ".autorecord": async (sock, msg, text, sender) => {
+    auto.autorecord = !auto.autorecord;
+
+    return sock.sendMessage(sender, {
+      text: `🎙️ AutoRecord: ${auto.autorecord ? "ON" : "OFF"}`
+    });
+  }
+
+};

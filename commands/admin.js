8f71dd2fc1303a7a9328fb8a5720b45af64f5304
Still <steveayoti01@gmail.com>
2026-06-06 20:21:49 +0300

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -1,1 +1,27 @@
+const config = {
+  owner: "2547XXXXXXXX@s.whatsapp.net"
+};
 
+function isAdmin(sender) {
+  return sender === config.owner;
+}
+
+module.exports = {
+
+  ".admin": async (sock, msg, text, sender) => {
+    if (!isAdmin(sender)) return;
+
+    return sock.sendMessage(sender, {
+      text: `
+👑 ADMIN PANEL
+
+.kick
+.promote
+.demote
+.ban
+.unban
+`
+    });
+  }
+
+};
